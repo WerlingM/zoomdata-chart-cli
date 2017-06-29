ae0e002531e5aa539388e6f44b91878269f1991f
Jonathan Avila <avilajonathan@gmail.com>
2017-06-29 00:51:11 -0400

    feat(edit): Prompt for additional editing
    When performing an edit on a chart, you'll now ask if you would like to do additional edits

--- src/questions/edit/index.ts
+++ src/questions/edit/index.ts
@@ -1,11 +1,13 @@
 import * as inquirer from 'inquirer';
 import { Visualization } from '../../@types/zoomdata';
 import { Config } from '../../commands/config';
+import { edit } from '../../commands/edit';
 import { strEnum } from '../../utilities';
 import * as componentQuestions from './components';
 import * as controlQuestions from './controls';
 import * as libraryQuestions from './libraries';
 import * as nameQuestions from './name';
+import * as variableQuestion from './variables';
 import * as visibilityQuestions from './visibility';
 
 const editOptions = strEnum([
@@ -42,26 +44,68 @@ function answerHandler(
   visualization: Visualization,
   serverConfig: Config,
 ): any {
-  switch (answers.editOptions) {
-    case editOptions.Components:
-      return componentQuestions.prompt(visualization, serverConfig);
-    case editOptions.Controls:
-      return controlQuestions.prompt(visualization, serverConfig);
-    case editOptions.Libraries:
-      return libraryQuestions.prompt(visualization, serverConfig);
-    case editOptions.Name:
-      return nameQuestions.prompt(visualization, serverConfig);
-    case editOptions.Visibility:
-      return visibilityQuestions.prompt(visualization, serverConfig);
-    // case editOptions.Variables:
-    //   return editOptions.Variables;
-  }
+  return new Promise((resolve, reject) => {
+    switch (answers.editOptions) {
+      case editOptions.Components:
+        return componentQuestions
+          .prompt(visualization, serverConfig)
+          .then(() => resolve())
+          .catch(error => reject(error));
+      case editOptions.Controls:
+        return controlQuestions
+          .prompt(visualization, serverConfig)
+          .then(() => resolve())
+          .catch(error => reject(error));
+      case editOptions.Libraries:
+        return libraryQuestions
+          .prompt(visualization, serverConfig)
+          .then(() => resolve())
+          .catch(error => reject(error));
+      case editOptions.Name:
+        return nameQuestions
+          .prompt(visualization, serverConfig)
+          .then(() => resolve())
+          .catch(error => reject(error));
+      case editOptions.Visibility:
+        return visibilityQuestions
+          .prompt(visualization, serverConfig)
+          .then(() => resolve())
+          .catch(error => reject(error));
+      case editOptions.Variables:
+        return variableQuestion
+          .prompt(visualization, serverConfig)
+          .then(() => resolve())
+          .catch(error => reject(error));
+    }
+  })
+    .then(() => confirmEdit(visualization, serverConfig))
+    .catch(error => Promise.reject(error));
 }
 
 function prompt(visualization: Visualization, serverConfig: Config) {
   return inquirer
     .prompt(questions)
     .then(answers => answerHandler(answers, visualization, serverConfig));
+}
+
+function confirmEdit(visualization: Visualization, serverConfig: Config) {
+  //noinspection ReservedWordAsName
+  const confirm: inquirer.Questions = [
+    {
+      default: false,
+      message: 'Would you like to make additional edits?:',
+      name: 'chartEdit',
+      type: 'confirm',
+    },
+  ];
+
+  return inquirer.prompt(confirm).then(confirmAnswers => {
+    if (confirmAnswers.chartEdit) {
+      return edit(visualization.name, serverConfig);
+    } else {
+      return Promise.resolve();
+    }
+  });
 }
 
 export { prompt };
